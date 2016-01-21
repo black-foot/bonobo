089e151051ecf6fbc5b806557a8e55111af0594d
black-foot <marcobeghi@gmail.com>
2016-01-21 22:02:56 +0000

    fns

--- functions.js
+++ functions.js
@@ -1,29 +1,61 @@
+var R = require('ramda')
 
+
+// An object that holds the current & running avg tweet frequencies.
 var data = {
-  hour:[],
-  day:[]}
-
-var bin = 1
-
-function dayData (x){
-
-  if(data.day.length < 24){
-
-    data.day.push(x)
-
-    return data.day
-
-  }else if(data.day.length = 24){
-
-    data.day.mean = (data.day.current + data.day.mean)/n
-
-    //Get the mean of the data.day array for the day
-
-    data.week.push(mean(data.day))
-
-    data.day=[]
-
+  minute:{
+    now:[],
+    avg:[]
+  },
+  hour:{
+    now:[],
+    avg:[]
+  },
+  day:{
+    now:[],
+    avg:[]
+  },
+  week:{
+    now:[],
+    avg:[]
+  },
+  month:{
+    now:[],
+    avg:[]
+  },
+  year:{
+    now:[],
+    avg:[]
+  }}
   }
+
+
+// A function that calculates the mean at each index of an array between current and running mean values
+function arrayMean(arr1,arr2,count){
+  for (i = 0; i < arr1.length; i++) {
+    var output = (arr1 + arr2 * count) / (count + 1);
+  }
+  return output
 }
 
-console.log(dayData(bin))
+// A function that handles tweet counts at a predefined bin inteval.
+//countPerInterval: The count of tweets in a predefined interval.
+//intervalCount: The time of the interval.
+function dataHandler (countPerInterval,intervalCount){
+
+  if(data.minute.now.length < 60000/intervalCount){
+
+    data.minute.now.push(countPerInterval)
+
+  }else if(data.minute.now.length = 60000/intervalCount){
+
+    var a = ++1
+    data.hour.now.push(R.mean(data.minute.now))
+    //Get the mean of the data.day array for the day
+    data.minute.avg = arrayMean(data.minute.now,data.minute.avg,)
+    data.minute.now = []
+  }
+  return data
+}
+
+console.log(dataHandler (countPerInterval,intervalCount))
